de4c6619fcf89f93e3d36cb7040c9f897986b031
Rémi Van Keisbelck <remi@rvkb.com>
2020-08-07 13:48:26 +0200

    deploy login

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
+npm install -g npm-login-noninteractive
+export NPM_USER=${CI_EMAIL}
+export NPM_PASS=${CI_API_KEY}
+
+npm-login-noninteractive
+
 cd ./core &&     \
 yarn publish &&  \
 cd ../tea-cup && \
