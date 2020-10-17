e82ecf0a32e23fcb9bd76ed44bc7d67db0eca462
Frank Wagner <agilecoderfrank@gmail.com>
2020-10-17 13:43:46 +0200

    improved implementation

--- core/src/TeaCup/Decode.ts
+++ core/src/TeaCup/Decode.ts
@@ -180,12 +180,12 @@ export class Decode {
 
   /**
    * Decoder for optinal object fields
-   * @param key the name of the field
+   * @param key the name of the optinal field
    * @param d the decoder for the field's value
    */
   static optionalField<T>(key: string, d: Decoder<T>): Decoder<T | undefined> {
     return Decode.andThen(
-      (value) => value.map<Decoder<T | undefined>>(() => Decode.field(key, d)).withDefault(Decode.succeed(undefined)),
+      (value) => value.map<Decoder<T | undefined>>(v => new Decoder<T>(() => d.decodeValue(v))).withDefault(Decode.succeed(undefined)),
       Decode.maybe(Decode.field(key, Decode.value))
     );
   }
