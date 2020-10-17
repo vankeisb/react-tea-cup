654047d23d1d5d479b689572e6107211f48b36f7
Frank Wagner <agilecoderfrank@gmail.com>
2020-10-17 13:34:47 +0200

    add optionalField decoder

--- core/src/TeaCup/Decode.ts
+++ core/src/TeaCup/Decode.ts
@@ -179,6 +179,18 @@ export class Decode {
   }
 
   /**
+   * Decoder for optinal object fields
+   * @param key the name of the field
+   * @param d the decoder for the field's value
+   */
+  static optionalField<T>(key: string, d: Decoder<T>): Decoder<T | undefined> {
+    return Decode.andThen(
+      (value) => value.map<Decoder<T | undefined>>(() => Decode.field(key, d)).withDefault(Decode.succeed(undefined)),
+      Decode.maybe(Decode.field(key, Decode.value))
+    );
+  }
+
+  /**
    * Decoder for navigable object properties
    * @param keys a list of fields to navigate
    * @param d the decoder for the leaf value
