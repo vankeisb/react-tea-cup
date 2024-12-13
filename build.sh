8e07a8ec17902832b51b39fbd73627547efb2c93
Remi Van Keisbelck <remi@rvkb.com>
2024-12-13 10:19:12 +0100

    move to npm

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-yarn install &&     \
-yarn bomlint &&     \
+npm install &&     \
+npm run bomlint &&     \
 cd core &&          \
 ./build.sh &&       \
 cd ../tea-cup &&    \
 ./build.sh &&       \
 cd ../samples &&    \
-yarn test --watchAll=false && \
-yarn build
+npm run test && \
+npm build
