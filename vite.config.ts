9466be5fd14d09583ef016a9a466a58be70f00dc
Remi Van Keisbelck <remi@rvkb.com>
2025-09-29 11:30:44 +0200

    vite config optimizeDeps

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react({jsxRuntime: 'classic'})],
+  plugins: [react({ jsxRuntime: 'classic' })],
   build: {
     manifest: true,
+    commonjsOptions: {
+      include: [/tea-cup/, /node_modules/],
+    },
+  },
+  optimizeDeps: {
+    include: ['react-tea-cup'],
   },
 });
